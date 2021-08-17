f1d8f10dbed05c83f3ea9b35a182b1da9dd4053e
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2021-08-17 17:16:34 -0300

    Resolucao desafio 1

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,13 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const link = document.querySelector('[data-js="link"]')
+
+link.addEventListener('click', (e) => {
+ const app = document.querySelector('[data-js="app"]')
+ app.classList.toggle('aula01')
+});
